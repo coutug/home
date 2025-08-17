b97cc0f6ded204af0b3b68040f145d770f9ec487
coutug <coutug18@hotmail.com>
2025-08-17 17:10:56 -0400

    add some packages

--- home/laptop.nix
+++ home/laptop.nix
@@ -6,13 +6,34 @@
   home.homeDirectory = "/home/gabriel";
   home.stateVersion = "25.05";
 
+  programs = {
+    alacritty.enable = true;
+    bat.enable = true;
+    # fzf.enable = true; #TODO dependency
+    # git.enable = true;
+    htop.enable = true;
+    # k9s.enable = true;
+    # neovim.enable = true;
+    # tmux.enable = true;
+    # zsh.enable = true;
+    zoxide.enable = true;
+  };
+
   home.packages = with pkgs; [
+    duf
     home-manager
+    # fd #TODO dependency
+    # ripgrep #TODO dependency
+    tealdeer
+    lazygit
+    lazydocker
+    curlie
+    # jq #TODO dependency
+    github-cli
+    kubectl
+    # kustomize helm helmfile argocd fluxcd
+    # sops age yq-go trivy terraform rclone
+    # act crane nmap
   ];
 
-  programs = {
-    alacritty = {
-      enable = true;
-    };
-  };
 }
