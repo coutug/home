7b528a860964faf2cea0c238eb665de984c0037c
coutug <coutug18@hotmail.com>
2025-08-29 21:57:58 -0400

    fix k9s aliases

--- home/programs/k9s.nix
+++ home/programs/k9s.nix
@@ -4,14 +4,16 @@
     enable = true;
 
     aliases = {
-      dp  = "deployments";
-      sec = "v1/secrets";
-      jo  = "jobs";
-      cr  = "clusterroles";
-      crb = "clusterrolebindings";
-      ro  = "roles";
-      rb  = "rolebindings";
-      np  = "networkpolicies";
+      aliases = {
+        dp  = "deployments";
+        sec = "v1/secrets";
+        jo  = "jobs";
+        cr  = "clusterroles";
+        crb = "clusterrolebindings";
+        ro  = "roles";
+        rb  = "rolebindings";
+        np  = "networkpolicies";
+      };
     };
 
     plugin = {
