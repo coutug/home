1b1a4b192b8696cb8c0f76dea18dfd571ac88b6c
coutug <coutug18@hotmail.com>
2025-09-01 10:40:36 -0400

    common: update vscode keybindings and add atuin settings

--- home/common.nix
+++ home/common.nix
@@ -38,8 +38,15 @@
     atuin = {
       enable = true;
       enableZshIntegration = true;
+      daemon.enable = true;
+      themes."marineTheme"."marineTheme".name = "marine"; # fonctionne pas
       settings = {
         auto_sync = true;
+        sync_frequency = "20m";
+        style = "compact";
+        update_check = false;
+        invert = true;
+        enter_accept = true;
       };
     };
     bat.enable = true;
