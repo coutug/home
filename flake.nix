5bbd95b21b72ad939353dc8152172c14edc55b76
coutug <coutug18@hotmail.com>
2026-02-12 11:37:51 -0500

    update opencode to use chatgpt codex 3

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     opencode = {
-      url = "github:anomalyco/opencode?ref=v1.1.34";
+      url = "github:anomalyco/opencode?ref=v1.1.53";
     };
     disko = {
       url = "github:nix-community/disko";
