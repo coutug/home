02de1534519614d3d7b03c70b29c8e5ae5d2a57a
coutug <coutug18@hotmail.com>
2026-09-08 15:43:32 -0400

    udpate flake and oc

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     opencode = {
-      url = "github:anomalyco/opencode?ref=v1.15.4";
+      url = "github:anomalyco/opencode?ref=v1.18.29";
     };
     disko = {
       url = "github:nix-community/disko";
