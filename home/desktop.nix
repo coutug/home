91adfb1a526776f606acb0b0c92aa832a132d12d
coutug <coutug18@hotmail.com>
2026-09-08 19:25:25 -0400

    clean up old configs

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,14 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [
     ./common.nix
   ];
-
-  # audio device not working, installed with yay
-  # home.packages = with pkgs; [
-  #   reaper
-  #   reaper-sws-extension
-  #   reaper-reapack-extension
-  # ];
 
 }
